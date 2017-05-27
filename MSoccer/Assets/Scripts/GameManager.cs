c5cbd7eb10a7eb306391b66580def30178c613b1
DanielFM <daniFM333@gmail.com>
2017-05-27 12:57:14 +0200

    Multiple features added
    - Ball
    - Players
    - Score
    - Collisions +-

---
+++ MSoccer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Text scoreText;
+    public Rigidbody2D ballRB;
+
+    public int pointsP1;
+    public int pointsP2;
+
+	// Use this for initialization
+	void Start () {
+
+        scoreText.GetComponent<Text>();
+        ballRB.GetComponent<Rigidbody2D>();
+
+        pointsP1 = pointsP2 = 0;
+        scoreText.text = pointsP1 + " : " + pointsP2;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateScore(int player, int score) {
+        if(player == 1) {
+            pointsP1 = score;
+        }else if(player == 2) {
+            pointsP2 = score;
+        }
+        scoreText.text = pointsP1 + " : " + pointsP2;
+
+        ballRB.transform.position = Vector2.zero;
+        ballRB.velocity = Vector2.zero;
+    }
+}
---
+++ MSoccer/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+        player.GetComponent<GameObject>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    private void OnMouseDrag() {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        player.transform.position = mousePos;//Input.mousePosition;
+    }
+}
