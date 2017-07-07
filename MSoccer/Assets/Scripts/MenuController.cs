059fbcab923b4ba0c0fc0b701b33aaff1af30cc1
DanielFM <daniFM333@gmail.com>
2017-07-07 18:05:27 +0200

    Improvements in menus
    Credits to main menu. Created basic pause menu.

--- MSoccer/Assets/Scripts/MenuController.cs
+++ MSoccer/Assets/Scripts/MenuController.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+    public GameObject myCredits;
+
 	// Use this for initialization
 	void Start () {
+
+        myCredits.SetActive(false);
 		
 	}
 	
@@ -22,5 +26,12 @@ public class MenuController : MonoBehaviour {
     public void Exit() {
         Debug.Log("Quitting Game...");
         Application.Quit();
+    }
+
+    public void ShowHideCredits() {
+        if (myCredits.activeInHierarchy)
+            myCredits.SetActive(false);
+        else
+            myCredits.SetActive(true);
     }
 }
